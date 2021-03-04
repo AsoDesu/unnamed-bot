f0dfd34fe3461dac0b4bba4631cf172c6ecb2cdf
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-03-04 19:29:40 +0000

    dockerfile for hosting

--- src/commands/neko.ts
+++ src/commands/neko.ts
@@ -1,9 +1,11 @@
 import { Message } from "discord.js";
 import got from 'got'
 
+import hypixelApiGrabber from '../hypixel-api/hypixelApiGrabber'
+
 async function command(msg: Message, args: string[]) {
     var data = JSON.parse((await got('https://nekos.life/api/v2/img/neko')).body)
-    msg.channel.send(data.url)
+    msg.channel.send(data.url) 
 }
 
 export default command
